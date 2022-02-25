0b6d2f02d53ff54e1887bc496b38013cb52ae8b9
galiii <gali.brill.1408@gmail.com>
2022-02-25 18:48:23 +0200

    hotfix/1

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -5,7 +5,7 @@ const About = () => {
   return (
     <section className="about">
       <img src={initialAbout.image} alt={"About"} className="about__image" />
-      <div className="about__container_text">
+      <div className="about__container-text">
         <h2 className="about__title">{initialAbout.title}</h2>
         <p className="about__text">{initialAbout.text1}</p>
         <p className="about__text">{initialAbout.text2}</p>
